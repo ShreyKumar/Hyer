cb178ff3cc4a3b32d1a708e2fe41d67c7b326f42
XXXXXXXXXIII <tim.xie.2009@gmail.com>
2018-03-18 19:33:56 -0400

    Lists jobs when fetched

--- frontend/Hyer/components/Jobs.js
+++ frontend/Hyer/components/Jobs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import {View, Text} from "react-native";
 import {Container, Content, Card, CardItem} from "native-base";
 
@@ -7,6 +7,8 @@ export default class Jobs extends React.Component {
     super(props);
 
     this.testArr = [(<Text key="hello">Hello </Text>), (<Text key="there">There</Text>)];
+    this.state = {isLoading : true,
+                    jobData : []}
 
     fetch("https://hyer.herokuapp.com/jobs", {
       method: "GET",
@@ -19,23 +21,56 @@ export default class Jobs extends React.Component {
       console.log("start response");
       response.json().then((data) => {
         console.log(data)
+        this.setState({isLoading:false, jobData:data});
       })
-
-      console.log("end reponse");
+      console.log("end response");
     }).catch((error) => {
       console.error(error);
     })
   }
 
   render(){
+
+    if (this.state.isLoading) {
+        return (
+            <Container>
+                <Content>
+                    <Text>Loading...</Text>
+                </Content>
+            </Container>
+        );
+    }
+
     return (
       <Container>
         <Content>
           <Text>Jobs</Text>
-          {this.testArr}
+          <View>
+            { this.jobList() }
+          </View>
         </Content>
       </Container>
     );
+  }
+
+  jobList () {
+    return Object.keys(this.state.jobData).map(job => {
+        return (
+            // TODO implement touch-to-expand
+            <Card>
+                <CardItem header>
+                    <Text>Job Name: { this.state.jobData[job].name }</Text>
+                </CardItem>
+                <CardItem>
+                    <View>
+                        <Text>Address: Address Property?</Text>
+                        <Text>Pay: ${ this.state.jobData[job].pay }</Text>
+                        <Text>Duration: { this.state.jobData[job].duration }hours</Text>
+                    </View>
+                </CardItem>
+            </Card>
+        )}
+    )
   }
 
 }
