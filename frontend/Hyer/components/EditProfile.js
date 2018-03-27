28c18dc87514ace52592a5bbefb8f26c887ab795
Shreyansh Kumar <shreyansh.kumar@mail.utoronto.ca>
2018-03-27 00:30:01 -0400

    created layout for edit profile

--- frontend/Hyer/components/EditProfile.js
+++ frontend/Hyer/components/EditProfile.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {View, Text} from "react-native";
-import {Container, Content, Card, CardItem} from "native-base";
+import {View, Text, TextInput} from "react-native";
+import {Container, Content, Form, Item, Label, Input, Button, Card, CardItem} from "native-base";
 
 export default class EditProfile extends React.Component {
   constructor(props){
@@ -8,7 +8,48 @@ export default class EditProfile extends React.Component {
   }
 
   render() {
-    return (<Text>Edit Profile</Text>)
+    return (
+      <Container className="edit-profile">
+      <Content>
+        <Form>
+          <Item floatingLabel>
+            <Label>Username</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel>
+            <Label>Firstname</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel>
+            <Label>Lastname</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input />
+          </Item>
+          <Card style={{
+            marginTop: 30
+          }}>
+            <CardItem>
+              <TextInput style={{
+                width: "100%",
+                fontSize: 19
+              }}
+              multiline={true}
+              numberOfLines={6}
+              maxLength={300}
+              placeholder="Bio"
+              allowFontScaling />
+            </CardItem>
+          </Card>
+          <Button block style={{marginTop: 30}}>
+            <Text>Update</Text>
+          </Button>
+        </Form>
+      </Content>
+      </Container>
+    )
   }
 
 }
